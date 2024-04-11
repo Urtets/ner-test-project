d47eb01ade7f80f7a4566a518b848289179840c5
Urtets <urtets@gmail.com>
2024-04-11 13:40:25 +0300

    Initial commit

---
+++ src/main/java/com/example/nertestproject/NerTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.nertestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerTestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nertestproject/NerTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nertestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
